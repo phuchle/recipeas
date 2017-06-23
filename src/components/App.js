0de0c6fd2685f49bc87cf264e03b87c083c38c25
Phuc Le <le.phuc.h@gmail.com>
2017-06-22 22:44:21 -0700

    Refactor to stateless functional components.

--- src/components/App.js
+++ src/components/App.js
@@ -4,15 +4,13 @@ import RecipesList from '../containers/RecipesList';
 import '../App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Navigation />
-        <RecipesList />
-      </div>
-    );
-  }
+const App = (props) => {
+  return (
+    <div className="App">
+      <Navigation />
+      <RecipesList />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,18 +1,18 @@
 import React from 'react';
 
-export default class Navigation extends React.Component {
-  render() {
-    return (
-      <nav className="navbar navbar-default navbar-fixed-top">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="/">Recipeas</a>
-          <ul className="nav navbar-nav">
-            <li>
-              <a href="https://github.com/phuchle/recipeas">Source Code</a>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    );
-  }
+const Navigation = (props) => {
+  return (
+    <nav className="navbar navbar-default navbar-fixed-top">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="/">Recipeas</a>
+        <ul className="nav navbar-nav">
+          <li>
+            <a href="https://github.com/phuchle/recipeas">Source Code</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
+
+export default Navigation;
