783d5b3b87f0ca13923750fdd0f669de038b1ce9
Phuc Le <le.phuc.h@gmail.com>
2017-07-25 18:04:41 -0700

    Pull out plus button to its own component.

---
+++ src/components/PlusButton.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Button } from 'react-bootstrap';
+
+const PlusButton = () => (
+  <Button
+    bsStyle="primary"
+    style={{ marginLeft: '5px' }}
+    className="glyphicon glyphicon-plus"
+  />
+);
+
+export default PlusButton
