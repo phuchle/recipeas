7d6217b22b6c6a4546af213d3cb2393598cea1cf
Phuc Le <le.phuc.h@gmail.com>
2017-08-08 19:47:48 -0700

    Refactor Main so it is a stateless functional component.

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import RecipesList from '../containers/RecipesList';
 import ModifyTitle from '../containers/ModifyTitle';
@@ -7,83 +7,46 @@ import ReviewRecipe from './ReviewRecipe';
 import SearchIngredient from '../containers/SearchIngredient';
 import IngredientDetails from './IngredientDetails';
 
-class Main extends Component {
-  constructor(props) {
-    super(props);
+const Main = (props) => {
+  return (
+    <Switch>
+      <Route exact path="/" component={RecipesList} />
 
-    this.state = {
-      title: '',
-      allergens: '',
-      servings: '',
-    };
+      <Route path="/add-recipe" render={() => (
+        <ModifyTitle
+          nextButton={true}
+          nextButtonStyle={{
+            marginBottom: '10px',
+            marginTop: '10px'
+          }}
+        />
+      )} />
 
-    this.handleIngredientDetailNextButton = this.handleIngredientDetailNextButton.bind(this);
-  }
-  handleIngredientDetailNextButton(newIngredient) {
-    this.setState({
-      ...this.state,
-      ingredients: [
-        ...this.state.ingredients,
-        newIngredient
-      ]
-    });
-  }
-  resetState() {
-    this.setState({
-      title: '',
-      allergens: '',
-      servings: '',
-      ingredients: []
-    });
-  }
-  render() {
-    return (
-      <Switch>
-        <Route exact path="/" component={RecipesList} />
+      <Route path="/add-ingredients" render={() => (
+        <ModifyIngredients
+          nextButton={true}
+          plusButton={true}
+          nextButtonStyle={{ marginBottom: '10px' }}
+        />
+      )} />
 
-        <Route path="/add-recipe" render={() => (
-          <ModifyTitle
-            nextButton={true}
-            nextButtonStyle={{
-              marginBottom: '10px',
-              marginTop: '10px'
-            }}
-          />
-        )} />
+      <Route path="/search-ingredient" component={SearchIngredient} />
 
-        <Route path="/add-ingredients" render={() => (
-          <ModifyIngredients
-            nextButton={true}
-            plusButton={true}
-            nextButtonStyle={{ marginBottom: '10px' }}
-          />
-        )} />
+      <Route path="/ingredient-details" render={({ location }) => (
+        <IngredientDetails
+          location={location}
+        />
+      )} />
 
-        <Route path="/search-ingredient" component={SearchIngredient} />
-
-        <Route path="/ingredient-details" render={({ location }) => (
-          <IngredientDetails
-            location={location}
-            handleAddIngredient={this.handleIngredientDetailNextButton}
-          />
-        )} />
-
-        <Route path="/review" render={() => (
-          <ReviewRecipe
-            title={this.state.title}
-            servings={this.state.servings}
-            allergens={this.state.allergens}
-            handleTitleChange={this.handleTitleChange}
-            handleServingsChange={this.handleServingsChange}
-            handleAllergensChange={this.handleAllergensChange}
-            buttonStyle={{
-            marginTop: '20px',
-            marginBottom: '10px'
-          }} />
-        )} />
-      </Switch>
-    );
-  }
+      <Route path="/review" render={() => (
+        <ReviewRecipe
+          buttonStyle={{
+          marginTop: '20px',
+          marginBottom: '10px'
+        }} />
+      )} />
+    </Switch>
+  );
 };
 
 export default Main;
